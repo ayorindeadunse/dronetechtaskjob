f76574d2505be03fed51a4ec68b99204a8c28290
adunseayorinde <adunseayorinde@gmail.com>
2022-08-18 14:45:34 +0100

    updated Main class

--- src/com/ayorinde/dronetechjob/Main.java
+++ src/com/ayorinde/dronetechjob/Main.java
@@ -1,8 +1,24 @@
 package com.ayorinde.dronetechjob;
 
+import com.ayorinde.dronetechjob.eventlog.ScheduledTask;
+
+import java.util.Timer;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws InterruptedException {
+        // write your code here
+
+        Timer t = new Timer();
+        ScheduledTask st = new ScheduledTask();
+        t.schedule(st,0,2000);
+        for (int i = 0; i <= 86400; i++) {
+            System.out.println("Task Running....");
+            Thread.sleep(5000);
+            if (i == 86400) {
+                System.out.println("Application Terminates");
+                System.exit(0);
+            }
+        }
     }
 }
